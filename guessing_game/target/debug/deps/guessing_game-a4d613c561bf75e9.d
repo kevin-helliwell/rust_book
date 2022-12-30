41f82e62899155ac262776d144c23b2c427eccf6
Kevin Helliwell <kbh789@gmail.com>
2022-12-29 19:07:53 -0500

    gitignore cleanup

--- guessing_game/target/debug/deps/guessing_game-a4d613c561bf75e9.d
+++
@@ -1,5 +0,0 @@
-/home/kevinh/Documents/rust_projects/guessing_game/target/debug/deps/guessing_game-a4d613c561bf75e9: src/main.rs
-
-/home/kevinh/Documents/rust_projects/guessing_game/target/debug/deps/guessing_game-a4d613c561bf75e9.d: src/main.rs
-
-src/main.rs:
